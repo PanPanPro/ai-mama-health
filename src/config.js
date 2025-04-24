c32726e5edc2d581d9390cbba55f6afe589d1d17
PanPanPro <matthew@landgroup.vip>
2025-04-24 12:58:00 +0800

    config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,9 @@
-config.js
+const OpenAI = require('openai');
+require('dotenv').config();
+
+const openai = new OpenAI({
+  apiKey: process.env.DEEPSEEK_API_KEY,
+  baseURL: 'https://api.deepseek.com',
+});
+
+module.exports = openai;
